154cbf10ef208b25cccbe7ba43a59ec7a291de16
thejunglife <thejunglife@gmail.com>
2019-12-03 00:07:36 -0800

    adjusted  dotenv

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize('mysql://root:password@localhost/burger2_db')
+// module.exports = new Sequelize('mysql://root:password@localhost/burger2_db')
 
-// module.exports = new Sequelize(process.env.NODE_ENV ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
+module.exports = new Sequelize(process.env.NODE_ENV ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
+
